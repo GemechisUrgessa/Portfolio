2fe84fec1b065022205d3764a02ef986860c6407
GemechisUrgessa <gemechis.urgessa@aait.edu.et>
2023-12-04 23:03:29 +0300

    some changes made

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
+// import "animate.css"; // Import animate.css for animations
+// import "./AboutCard.css"; // Import a custom CSS file for additional styling
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className="quote-card-view animate__animated animate__fadeIn">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Gemechis Urgessa </span>
-            from <span className="purple"> Ethiopia.</span>
-            <br /> I am a final year student pursuing B.sc in Software Engineering from Addis Ababa Institute of Technology.
+            Hi Everyone, I am <span className="purple">Gemechis Urgessa</span>
+            from <span className="purple">Ethiopia.</span>
+            <br /> I am a final year student pursuing B.sc in Software
+            Engineering from Addis Ababa Institute of Technology.
             <br />
-            Additionally, I am currently employed as a software Engineer and Researcher  at UM.
+            Additionally, I am currently employed as a software Engineer and
+            Researcher at UM.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -29,8 +33,8 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          <p className="motto">
+            "Strive to create things that can make a difference!"
           </p>
           <footer className="blockquote-footer">Gemechis Urgessa</footer>
         </blockquote>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -8,6 +8,7 @@ import {
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+// import "./Home2.css"; // Import a custom CSS file for additional styling
 
 function Home2() {
   return (
@@ -15,56 +16,33 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
+            <h1 className="heading-intro">
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-                Through my passion for programming, I have gained extensive experience in a variety of programming languages, 
-                including <i>
-                <b className="purple"> C++, C, Dart, Javascript, HTML, CSS, Python, C#, Java, Rust, GO, React, Node.js and DJango. </b>
-              </i> I am particularly interested in 
-                developing new web technologies and products, as well as exploring the potential of <b className="purple">blockchain and AI.</b> 
-                 I have a strong background in <b className="purple"> MERN stack development</b>, and I am proficient in modern Javascript libraries 
-                 and frameworks such as <b className="purple"> React.js and Next.js.</b> I am always eager to learn and expand my skillset,
-                  and I am committed to delivering high-quality products that meet the needs of my clients and users.
-              {/* <br />
-              <br />I am fluent in classics like
-              
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain and AI.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">MERN stack</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i> */}
+              Passionate in programming, I excel in{" "}
+              <span className="purple">
+                C++, Dart, Javascript, GO, Rust, Java, C , Python
+              </span>
+              , and more. My interest lies in{" "}
+              <span className="purple">web technologies</span>,{" "}
+              <span className="purple">blockchain</span>, and{" "}
+              <span className="purple">AI</span>. Proficient in{" "}
+              <span className="purple">MERN stack</span> and frameworks like{" "}
+              <span className="purple">React.js</span>. Committed to delivering
+              quality products, I am on a continual journey of learning and
+              growth.
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
+          <Col md={4} className="my-avatar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
-        
       </Container>
     </Container>
   );
 }
+
 export default Home2;
