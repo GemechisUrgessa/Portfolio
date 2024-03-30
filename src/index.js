1b3607a3e039a5e75032049110af4663bb178515
Gemechis <91595853+GemechisUrgessa@users.noreply.github.com>
2024-03-30 13:57:42 +0000

    reverted--

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
   document.getElementById("root")
 );
